ef3cc1dd02412e016dbd1d590d7d311aafaf7eb0
IPLit <31268854+IPLit@users.noreply.github.com>
2022-10-03 19:11:34 +0530

    Update njs.ts

--- src/njs.ts
+++ src/njs.ts
@@ -1,5 +1,5 @@
 const delegateRepository: { [key: string]: string } = {
-  'devlite1': 'dev.bahmnilite.in',
+  'bahmni-dev': 'dev.bahmnilite.in',
   'clinic1': 'clinic1.bahmnilite.in',
   'clinic2': 'clinic2.bahmnilite.in',
 };
