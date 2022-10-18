2e7607eb1a3aefb9c449d75d76712bfc0caa026e
IPLit <31268854+IPLit@users.noreply.github.com>
2022-10-18 14:33:14 +0530

    Update njs.ts

--- src/njs.ts
+++ src/njs.ts
@@ -1,6 +1,6 @@
 const delegateRepository: { [key: string]: string } = {
   'Shree Clinic': 'shreeclinic.bahmnilite.in',
-  'Clinic1': 'clinic1.bahmnilite.in',
+  'IN2710000189': 'clinic1.bahmnilite.in',
   'clinic2': 'clinic2.bahmnilite.in',
 };
 
